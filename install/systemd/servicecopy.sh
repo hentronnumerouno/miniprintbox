ffce2f1aec8c3c93907c30a7a67534c1a0fe43ed
henry <henryrendler@gmail.com>
2023-08-08 00:44:47 -0700

    dev - no camera and no serial

--- install/systemd/servicecopy.sh
+++
@@ -1,5 +0,0 @@
-cp miniprint.service /lib/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable miniprint.service
-sudo systemctl start miniprint.service
-sudo systemctl status miniprint.service
